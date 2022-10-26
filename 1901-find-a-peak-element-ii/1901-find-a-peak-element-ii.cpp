e59bff0526d863d9750e2dc08cb761d6c45c0853
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-10-26 18:38:40 +0530

    Time: 170 ms (99.02%), Space: 46.4 MB (12.18%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,60 +1,52 @@
 class Solution {
 public:
-    int findmax(vector<vector<int>> &mat , int r)
-    {
-        int maxe=INT_MIN;
-        int idx=0;
-        for(int i=0;i<mat[0].size();i++)
-        {
-            
-            if(maxe<mat[r][i])
-            {
-                maxe=mat[r][i];
-                idx=i;
-            }
-        }
-        return idx;
-    }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         vector<int>a;
         int l=0;
         int m=mat.size();
+        int n=mat[0].size();
         int r=m-1;
         while(l<=r)
-        {   
+        {   int h=0,maxi=INT_MIN;
             int mid=(l+r)/2;
-            int h=findmax(mat,mid);
-             int val=mat[mid][h];
-            
+            for(int i=0;i<n;i++)
+            {    
+                maxi=max(maxi,mat[mid][i]);
+            }
+            for(int i=0;i<n;i++)
+            {
+                if(maxi==mat[mid][i])
+                    h=i;
+            }
             if(mid==0)
             {
-                if(val>mat[mid+1][h])
+                if(maxi>mat[mid+1][h])
                 {
                     a.push_back(mid);
                     a.push_back(h);
                     break;
                 }
-                else if(val>mat[mid+1][h])
+                else if(maxi>mat[mid+1][h])
                 r=mid-1;
                 else
                 l=mid+1;
             }
             else if(mid==r)
             {
-                if(val>mat[mid-1][h])
+                if(maxi>mat[mid-1][h])
                 {
                     a.push_back(mid);
                     a.push_back(h);
                     break;
                 }
-                else if(mat[mid-1][h]>val)
+                else if(mat[mid-1][h]>maxi)
                 r=mid-1;
                 else
                 l=mid+1;
             }
             else if(mid>0 && mid<r )
             {
-                if(val>mat[mid-1][h] && val>mat[mid+1][h])
+                if(maxi>mat[mid-1][h] && maxi>mat[mid+1][h])
                  {
                     a.push_back(mid);
                     a.push_back(h);
