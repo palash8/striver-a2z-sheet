0006d44255140259d2d45de37b0cd3a90c6fa38e
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-10-26 18:29:14 +0530

    Time: 183 ms (95.75%), Space: 46.4 MB (12.18%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-  int findmax(vector<vector<int>> &mat , int r)
+    int findmax(vector<vector<int>> &mat , int r)
     {
         int maxe=INT_MIN;
         int idx=0;
@@ -15,58 +15,60 @@ public:
         }
         return idx;
     }
-       
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        // Using Binary Search
-        int m=mat.size() , n=mat[0].size();
-        int l=0 , r=m-1;
-        
+        vector<int>a;
+        int l=0;
+        int m=mat.size();
+        int r=m-1;
         while(l<=r)
-        {
-            int mid=(l+(r-l)/2);
-            
-            int c=findmax(mat , mid);
-            
-            int val=mat[mid][c];
+        {   
+            int mid=(l+r)/2;
+            int h=findmax(mat,mid);
+             int val=mat[mid][h];
             
             if(mid==0)
             {
-                if(mat[mid+1][c] < val)
+                if(val>mat[mid+1][h])
                 {
-                    return {mid , c};
+                    a.push_back(mid);
+                    a.push_back(h);
+                    break;
                 }
-            }
-            else
-            if(mid==m-1)
-            {
-                if(mat[mid-1][c]<val)
-                {
-                    return {mid , c};
-                }
-            }
-            else
-            if(val>mat[mid+1][c] && val>mat[mid-1][c])
-            {
-                return {mid , c};
-            }
-            
-            
-            if( val<mat[mid+1][c]) 
-            {                        
-                                     
+                else if(val>mat[mid+1][h])
+                r=mid-1;
+                else
                 l=mid+1;
             }
-            else
-            {                     
+            else if(mid==r)
+            {
+                if(val>mat[mid-1][h])
+                {
+                    a.push_back(mid);
+                    a.push_back(h);
+                    break;
+                }
+                else if(mat[mid-1][h]>val)
                 r=mid-1;
+                else
+                l=mid+1;
             }
+            else if(mid>0 && mid<r )
+            {
+                if(val>mat[mid-1][h] && val>mat[mid+1][h])
+                 {
+                    a.push_back(mid);
+                    a.push_back(h);
+                    break;
             
-            
-        }
-        
-        return {-1 , -1};
-        
+                 }
+                else if(mat[mid-1][h]>mat[mid+1][h])
+                r=mid-1;
+                else
+                l=mid+1;
+           }
+                    
+         }
+                
+     return a; 
     }
-        
-               
 };
