9c0b02b81d9d9363d0b21fcdb683ab6ab40906bc
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-10-30 11:52:57 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (89.47%) - LeetHub

---
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long SumOfDigits(long n)
+    {
+      long sum = 0;
+      while(n > 0)
+      {
+        sum += (n % 10);
+        n /= 10;
+      }
+      return sum;
+    }   
+    long long makeIntegerBeautiful(long long n, int target) {
+        long mul = 1;
+        long add = 0;
+        while(SumOfDigits(n + add) > target)
+        {
+          mul = mul * 10;
+          add = mul - n % mul;
+        }
+        return add;
+        
+    }
+};
