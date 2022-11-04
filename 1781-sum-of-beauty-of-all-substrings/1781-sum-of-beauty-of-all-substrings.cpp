77534e7f2baf25cbc841431f79d36934ee211ab6
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-11-04 19:47:27 +0530

    Time: 874 ms (37.04%), Space: 8.7 MB (60.58%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int beauty(vector<int>&alpha)
+    {
+        int mf=-1;
+        int lf=1e9;
+        for(int i=0;i<26;i++)
+        {
+            mf=max(mf,alpha[i]);
+            if(alpha[i]>=1)
+                lf=min(lf,alpha[i]);
+        }
+        return mf-lf;
+    }
+    int beautySum(string s) {
+        int n=s.size();
+        int res=0;
+        for(int i=0;i<n;i++)
+        {
+            vector<int>alpha(26,0);
+            for(int j=i;j<n;j++)
+            {
+                alpha[s[j]-'a']++;
+                res=res+beauty(alpha);
+            }
+        }
+        
+     return res;   
+        
+    }
+};
