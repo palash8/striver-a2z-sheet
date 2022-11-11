fdcfb0d46ff3a421287217a53f12e773ee250c13
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-11-11 11:34:50 +0530

    Time: 284 ms (83.25%), Space: 118.1 MB (48.93%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -28,18 +28,20 @@ public:
           ListNode* slow = head;
           ListNode* fast = head;
         
-    while(fast->next!=NULL&&fast->next->next!=NULL) {
+       while(fast->next!=NULL&&fast->next->next!=NULL)
+       {
         slow = slow->next;
         fast = fast->next->next;
-    }
-        slow->next = reverse(slow->next);
+       }
+       slow->next = reverse(slow->next);
        slow = slow->next;
-        ListNode* dummy = head;
-        while(slow!=NULL) {
-        if(dummy->val != slow->val) return false;
-        dummy = dummy->next;
-        slow = slow->next;
-    }
-    return true;
+       ListNode* dummy = head;
+       while(slow!=NULL) 
+        {
+           if(dummy->val != slow->val) return false;
+           dummy = dummy->next;
+           slow = slow->next;
+        }
+        return true;
      }
 };
