08c08858e09ef282cace14786b92da5f6c71bb86
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-12-03 09:08:35 +0530

    Time: 535 ms (5.05%), Space: 21.1 MB (6.53%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -4,21 +4,30 @@ public:
         int count=0;
         if(word1.size()==word2.size())
         {
-          vector<int>w1(26,0),w2(26,0);
-          set<char>s1,s2;
-            for(char c : word1)
+            map<char,int>s1;
+            map<char,int>s2;
+            set<int>l2,l1;
+            vector<int>v2,v1;
+            
+            for(int i=0;i<word1.size();i++)
             {
-                w1[c-'a']++;
-                s1.insert(c);
+                l1.insert(word1[i]);
+                s1[word1[i]]++;
             }
-           for(char c : word2)
+            for(int i=0;i<word2.size();i++)
             {
-                w2[c-'a']++;
-                s2.insert(c);
+                l2.insert(word2[i]);
+                s2[word2[i]]++;
             }
-            sort(w1.begin(),w1.end());
-            sort(w2.begin(),w2.end());
-            return w1==w2 &&s1==s2;
+            for(auto [k,v]:s1)v1.push_back(v);
+             for(auto [k,v]:s2)v2.push_back(v);
+            
+            sort(v1.begin(),v1.end());
+              
+            sort(v2.begin(),v2.end());
+            
+            if(v1==v2 && l1==l2)
+                return true;
           
         }
         return false;
