f70bc6f6e0a24f75647980f22111b2575c752a78
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-12-06 15:44:56 +0530

    Added solution - LeetHub

--- Prime Factorization using Sieve. - GFG/prime-factorization-using-sieve..cpp
+++ Prime Factorization using Sieve. - GFG/prime-factorization-using-sieve..cpp
@@ -11,17 +11,24 @@ vector<int>spf(n+1);
 
 class Solution {
   public:
+  //CHECK ON STRIVER VIDEON THIS
+  //seive is storing values from 1 to N and we store it with its smallest prime factor
     void sieve() {
+        //first storing every number with it only
         for(int i=1;i<=n;i++)
         {
             spf[i]=i;
         }
+        //then storing numbers with its smallest prime factor,running loop till sqrt(n),as prime 
+        //factors a number are less than equal to sqrt(n)
         for(int i=2;i*i<=n;i++)
-        {
+        {   //if condition for limiting the loop to check for prime numbers only
            if(spf[i]==i)
-           {
+           {   //this loop for 2 goes as->4,6,8,10....for 3 goes as->9,12,15..
                for(int j=i*i;j<=n;j=j+i)
-               {
+               { //change the value with the new smallest prime value..
+                 //this if condition ensures that only position with value itself must be changed
+                 //here it goes in from smallest to largest
                    if(spf[j]==j)
                    spf[j]=i;
                }
@@ -35,8 +42,9 @@ class Solution {
         // Write your code here
         vector<int>a;
         sieve();
+        //run the loop till N!=1 as after that no prime factors
         while(N!=1)
-        {
+        {   //in each step we are reducing the input N and then checking for that N
             a.push_back(spf[N]);
             N=N/spf[N];
         }
