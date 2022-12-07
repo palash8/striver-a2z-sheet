ebd0f15f3ce19ed22da02505917c8426e896041f
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-12-07 22:01:54 +0530

    Time: 11 ms (62.45%), Space: 10.9 MB (6.16%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int>s1,s2;
         vector<int>v;
         vector<int>::iterator ip;
-        if(nums1.size()<=nums2.size())
+        for(auto i:nums1)
+            s1.insert(i);
+        for(auto i:nums2)
+            s2.insert(i);
+        for(auto i:nums1)
         {
-            for(auto i:nums1)
-            {
-                for(auto j:nums2)
-                {
-                    if(i==j)
-                        v.push_back(i);
-                }
-            }
+            if(s2.find(i)!=s2.end())
+                v.push_back(i);
         }
-        else
-        {
-            for(auto i:nums2)
-            {
-                for(auto j:nums1)
-                {
-                    if(i==j)
-                        v.push_back(i);
-                }
-            }
-            
-        }
-        
-    sort(v.begin(),v.end());
-    // Using std::unique
-    ip = std::unique(v.begin(), v.end());
-
-    v.resize(std::distance(v.begin(), ip));
+        sort(v.begin(),v.end());
+  
+       // Using std::unique
+       ip = std::unique(v.begin(), v.end());
+      // Resizing the vector so as to remove the undefined terms
+       v.resize(std::distance(v.begin(), ip));
         return v;
+        
         
     }
 };
