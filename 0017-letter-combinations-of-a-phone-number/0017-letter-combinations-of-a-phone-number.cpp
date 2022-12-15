290697b679e48452d9016b88baa9713775d38a0f
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-12-15 18:41:51 +0530

    Time: 4 ms (31.08%), Space: 6.5 MB (82.22%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    void solve(string digits, vector<string> &ans, string options[], int i, string output){
-        if(i == digits.length()){
+    void solve(int n, vector<string> &ans, string options[], string output){
+        if(n==0){
             ans.push_back(output);
             return;
         }
-        string s1 = options[digits[i] - '0'];
+        int n1=n%10;
+        string s1=options[n1];
         for(int j=0;j<s1.length(); j++)
-            solve(digits, ans, options, i+1, output+s1[j]);
+            solve(n/10, ans, options, s1[j]+output);
     }
     
     vector<string> letterCombinations(string digits) {
+        int sum=0;
+        for(int i=0;i<digits.length();i++)
+        {
+            sum=sum*10+(digits[i]-'0');
+        }
         vector<string> ans;
         if(digits.length() == 0)
             return ans;
         string options[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        solve(digits, ans, options, 0, "");
+        solve(sum, ans, options, "");
         return ans;
     }
 };
