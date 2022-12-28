b2554697844d4142be6734620e240b7497daa710
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-12-28 22:53:02 +0530

    Time: 501 ms (48.08%), Space: 57.3 MB (67.23%) - LeetHub

---
+++ 2512-reward-top-k-students/2512-reward-top-k-students.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> topStudents(vector<string>& positive_feedback, vector<string>& negative_feedback, vector<string>& report, vector<int>& student_id, int k) {
+        set <string> pos(positive_feedback.begin(), positive_feedback.end());
+        set <string> neg(negative_feedback.begin(), negative_feedback.end());
+        
+        int n = student_id.size();
+        vector <array <int, 2>> store;
+        
+        for(int i=0; i<n; i++){
+            int pts = 0;
+            string s = report[i], temp = s.substr(0, 1);
+            for(int i=1; i<(int)s.length(); i++){
+                if(s[i] == ' '){
+                    if(pos.find(temp) != pos.end()){
+                        pts += 3;
+                    }
+                    else if(neg.find(temp) != neg.end()){
+                        pts--;
+                    }
+                    temp.clear();
+                }
+                else{
+                    temp.push_back(s[i]);
+                }
+            }
+            
+            if(pos.find(temp) != pos.end()){
+                pts += 3;
+            }
+            else if(neg.find(temp) != neg.end()){
+                pts--;
+            }
+            
+            store.push_back({pts, -student_id[i]});
+        }
+        
+        vector <int> ans;
+        sort(store.rbegin(), store.rend());
+        for(int i=0; i<min(k, (int)store.size()); i++){
+            ans.push_back(-store[i][1]);
+        }
+        
+        return ans;
+    }
+};
