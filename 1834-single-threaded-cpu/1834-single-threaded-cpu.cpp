576e55f519e5027c1eabe7b61f4fa4f7d6523c76
Palash Yadav <43900443+palash8@users.noreply.github.com>
2022-12-29 15:11:54 +0530

    Time: 681 ms (83.86%), Space: 129.3 MB (68.25%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -5,16 +5,20 @@ public:
         int N = A.size();
         long time = 0;
         int i = 0;
-        for (int i = 0; i < N; ++i){
+        for (int i = 0; i < N; ++i)
+        {
             A[i].push_back(i);
         }
         sort(begin(A), end(A));
         vector<int> ans;
-        while (i < N || pq.size()){
-            if (pq.empty()) {
+        while (i < N || pq.size())
+        {
+           if (pq.empty()) 
+             {
                 time = max(time, (long)A[i][0]);
-            }
-            while (i < N && time >= A[i][0]){
+             }
+            while (i < N && time >= A[i][0])
+            {
                 pq.push({A[i][1],A[i][2]});
                 ++i;
             }
