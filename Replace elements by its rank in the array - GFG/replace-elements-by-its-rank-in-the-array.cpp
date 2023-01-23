12cdefbed798f35b0cffad1bc79702ab11fb8d76
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-01-23 16:53:53 +0530

    Added solution - LeetHub

---
+++ Replace elements by its rank in the array - GFG/replace-elements-by-its-rank-in-the-array.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+
+    vector<int> replaceWithRank(vector<int> &arr, int n){
+        vector<int>ans;
+    map<int, int>mp;
+	int temp = 1;
+	int brr[n];
+	for (int i = 0; i < n; i++) {
+		brr[i] = arr[i];
+	}
+	sort(brr, brr + n);
+	for (int i = 0; i < n; i++) {
+		//if element is previously not store in the map
+		if (mp[brr[i]] == 0) {
+			mp[brr[i]] = temp;
+			temp++;
+		}
+	}
+	for (int i = 0; i < n; i++) 
+	{
+		ans.push_back(mp[arr[i]]);
+	}
+
+   return ans;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while(t--){
+        //Input
+
+       int n; cin >> n;
+       vector<int> vec(n);
+       for(int i = 0;i<n;i++) cin >> vec[i];
+
+        Solution obj;
+        vector<int> ans = obj.replaceWithRank(vec,n);
+        for(int i = 0;i<n;i++) cout << ans[i] << " ";
+        cout << endl;
+        
+
+        
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
