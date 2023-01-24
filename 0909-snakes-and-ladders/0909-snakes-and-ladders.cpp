466ec93b45e208c16c77eb6de60ba83454b40bf8
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-01-24 10:45:57 +0530

    Time: 15 ms (93.78%), Space: 12.2 MB (99.36%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -22,30 +22,46 @@ public:
     int snakesAndLadders(vector<vector<int>>& board) {
           int n = board.size();
           int steps = 0;
+        //queue for storing current positions for options
           queue<int>q;
+        //a visited array for all the points
           bool visited[n][n];
-        for(int i=0;i<n;i++)
-        {
+        //intializing  the bool array with false,otherwise may cause error like it did in                 //previous submission
+          for(int i=0;i<n;i++)
+          {
             for(int j=0;j<n;j++)
                 visited[i][j]=false;
-        }
+          }
+         //push 1st element in queue
           q.push(1);
+        //mark it visited
           visited[n-1][0] = true;
+        //run the loop till q is not empty
           while(!q.empty())
           {
             int size = q.size();
         
             for(int i =0; i <size; i++)
             {
+                //start a bfs traversal picking first element and its 6 options and then pooping 
+                //it out
                 int x = q.front();
                 q.pop();
+                //if alerady reach the last point return steps
                 if(x == n*n) return steps;
+                //run for every possible path from the node,as total 6 numbers can be generated                   //from the dice
                 for(int k=1; k <=6; k++)
                 {
                   if(k+x > n*n) break;
+                    
+                //tricky task is to find the coordinates
+                    
                   int *pos = findCoordinates(k+x, n);
                   int r = pos[0];
                   int c = pos[1];
+                    
+                  //if alerady visited continue the loop
+                    
                   if(visited[r][c] == true) 
                      continue;
                   visited[r][c] = true;
