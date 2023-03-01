70413d8658de0adcacad9d56538aafd3db554932
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-03-01 15:58:57 +0530

    Time: 16 ms (99.90%), Space: 25.1 MB (60.79%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,12 +3,19 @@ public:
     void rotate(vector<int>& nums, int k) 
     {
         //Method1
-        int n=nums.size();
+       /* int n=nums.size();
         vector<int>copy(nums.begin(),nums.end());
         for(int i=0;i<n;i++)
         {
             nums[(i+k)%n]=copy[i];
-        }
-      
+        }*/
+        
+        //Method 2
+       int n=nums.size();
+        k=k%n;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        
     }
 };
