c58f1921ca539a04af908b121f9c08c2a5d0bf37
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-03-04 12:48:53 +0530

    Time: 19 ms (60.77%), Space: 12.2 MB (6.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -7,14 +7,13 @@ public:
         vector<int>ans;
         for(int i=0;i<nums.size();i++)
         {
-            if(map.find(target-nums[i])!=map.end() and i!=map[target-nums[i]])
+            if(map.find(target-nums[i])!=map.end())
             {
                 ans.push_back(map[target-nums[i]]);
                 ans.push_back(i);
             }
              map[nums[i]]=i;
         }
-        sort(ans.begin(),ans.end());
         return ans;
     }
 };
