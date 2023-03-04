31b606106dd647d1fdbc0eea5f40a0e631d8f5e5
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-03-04 09:19:16 +0530

    Time: 88 ms (86.95%), Space: 41.5 MB (60.43%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
+    //we are using prefix sum approach here as sliding window and 2 pointer approach do not work here
+    //what we do is we traverse through the array and add values to sum variable,check if sum==k then increment the count
+    //and we also store the new sum in unordered map 
+    //we are using unordered map here to check for sum-k is present in map,like if current sum is 13 and k=9 then if 13-9=4 is alerady present in the map,then we have an subarray equal to k=9
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int>m;
-        int count=0,sum=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
+        int sum=0,count=0;
+        unordered_map<int,int>map;
+        for(auto i:nums)
         {
-            sum+=nums[i];
+            sum+=i;
             if(sum==k)count++;
-            if(m.find(sum-k)!=m.end())
-            {
-                count=m[sum-k]+count;
-            }
-            if(m.find(sum)!=m.end())
-            {
-             m[sum]++;
-            }else 
-            {
-                m[sum]=1;
-            } 
+            
+            if(map.find(sum-k)!=map.end())
+                count=map[sum-k]+count;
+            map[sum]++;
             
         }
-     return count;   
+       return count; 
     }
-
 };
