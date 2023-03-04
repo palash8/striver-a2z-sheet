195977536ea1f359c2168c665fcd39bd368452e0
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-03-04 09:56:03 +0530

    Time: 23 ms (59.20%), Space: 17 MB (56.33%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int sum=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            sum^=nums[i];
-        }
-       return sum; 
+        for(auto i:nums)
+            sum^=i;
+        return sum;
+        
     }
 };
