38b85f3e50b9d8617c6ad82d0a8f508c48578180
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-03-16 10:59:13 +0530

    Time: 3 ms (86.08%), Space: 9 MB (31.90%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,45 +1,48 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n=nums.size();
-        int l=0,r=n-1,count=0;
-        if(n==1)
-        {
+        int l=0,r=nums.size()-1;
+        int peak=0;
+        if(r==0)
             return 0;
-        }
         while(l<=r)
         {
-            int mid=(l+r)/2;
-            
-            if(mid>0 && mid<n-1)
+            int mid=l+(r-l)/2;
+            if(mid>0 and mid<nums.size()-1)
             {
-                if(nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1])
-                {
-                    count=mid;
-                    break;
-                }
-                else if(nums[mid+1]>nums[mid-1])
-                    l=mid+1;
-                else
-                    r=mid-1;
+              if(nums[mid]<nums[mid+1])
+                l=mid+1;
+              else if(nums[mid]<nums[mid-1])
+                r=mid-1;
+              else
+              {
+                 peak=mid;
+                 break;
+              }
             }
             else if(mid==0)
             {
-                if(nums[mid]>nums[mid+1])
-                    return 0;
-                 else
-                    return 1;
-            }
-            else if(mid==n-1)
-            {
-              if(nums[mid]>nums[mid-1])
-                    return n-1;
+                if(nums[mid]<nums[mid+1])
+                    l=mid+1;
                 else
-                    return n-2;  
+               {
+                 peak=mid;
+                 break;
+               }
             }
-                
-        }
-        return count;
-        
+            else if(mid==nums.size()-1)
+            {
+                if(nums[mid]<nums[mid-1])
+                    r=mid-1;
+                else
+                {
+                  peak=mid;
+                  break;
+                }
+            }
+    
+            
+         }
+        return peak;
     }
 };
