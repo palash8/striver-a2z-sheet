59bf08756fd67b9918bfb8519f1b974edb365d3c
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-03-16 10:18:10 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -1,5 +1,4 @@
 //{ Driver Code Starts
-
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -11,53 +10,56 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-	int fo(int arr[],int n,int x)
+int firstocc(int arr[], int n , int x )
+{
+    int l=0,r=n-1;
+    int res=-1;
+    while(l<=r)
+    {
+        int mid=l+(r-l)/2;
+        if(arr[mid]==x)
+        {
+            res=mid;
+            r=mid-1;
+        }
+        else if(arr[mid]<x)
+        {
+            l=mid+1;
+        }
+        else
+        r=mid-1;
+    }
+    return res;
+}
+int lastocc(int arr[], int n , int x )
+{
+    int l=0,r=n-1;
+    int res=-1;
+    while(l<=r)
+    {
+        int mid=l+(r-l)/2;
+        if(arr[mid]==x)
+        {
+            res=mid;
+            l=mid+1;
+        }
+        else if(arr[mid]<x)
+        {
+            l=mid+1;
+        }
+        else
+        r=mid-1;
+    }
+    return res;
+}
+	int count(int arr[], int n, int x) 
 	{
-	    int l=0,r=n-1,ans=0;
-	    while(l<=r)
-	    {
-	        int mid=(l+r)/2;
-	        if(arr[mid]==x)
-	        {
-	            ans=mid;
-	            r=mid-1;
-	        }
-	        else if(arr[mid]<x)
-	        l=mid+1;
-	        else
-	        r=mid-1;
-	    }
-	    return ans;
-	}
-	int lo(int arr[],int n,int x)
-	{
-	    int l=0,r=n-1,ans=-1;
-	    while(l<=r)
-	    {
-	        int mid=(l+r)/2;
-	        if(arr[mid]==x)
-	        {
-	            ans=mid;
-	            l=mid+1;
-	        }
-	        else if(arr[mid]<x)
-	        l=mid+1;
-	        else
-	        r=mid-1;
-	    }
-	    return ans;
-	}
-	
-	
-	
-	int count(int arr[], int n, int x) {
-	    
 	    // code here
-	    int l=fo(arr,n,x);
-	    int y=lo(arr,n,x);
-	    if(l==-1||y==-1)
-	    return 0;
-	    else return y-l+1;
+	    int f=firstocc(arr,n,x);
+        int l=lastocc(arr,n,x);
+        if(l==-1 || f==-1)
+        return 0;
+        return l-f+1;
 	}
 };
 
