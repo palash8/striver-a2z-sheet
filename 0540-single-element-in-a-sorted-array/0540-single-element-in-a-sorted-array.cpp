d89d912856c64a613efa0a5cb43bb186d4bace14
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-03-23 16:21:33 +0530

    Time: 29 ms (61.58%), Space: 22.3 MB (93.67%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n=nums.size();
+        //left half
+        //1st instance=even index(check the right side)
+        //2nd instance=odd index(check the left side if same or not)
+        
+        //Right half
+        //1st instance=odd index(check the left side)
+        //2nd instance=evem index(check the right side if same or not)
+        
+        //so instead of writing many case what we can do is use help of XOR operator
+        //XOR operator when number(even)XORED 1 results in next odd and vice versa
+        
+        //here we are shrking the search space based on indexes
+        
+         int n=nums.size();
         int l=0,r=n-2;
         while(l<=r)
         {
             int mid=(l+r)/2;
+            //checking left half
             if(nums[mid]==nums[mid^1])
             {
                 l=mid+1;
@@ -14,6 +28,7 @@ public:
                 r=mid-1;
         }
         return nums[l];
+        
         
     }
 };
