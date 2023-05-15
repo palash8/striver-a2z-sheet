b1e13cd64457f2485cb68911b5e638a48eb2f2af
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-05-15 12:30:12 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (96.25%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,20 +1,22 @@
 #define mod 1000000007
 class Solution {
 public:
-    long long power(long long n,long long p)
+    long long power(long long m,long long p)
     {
-        if(p==0)return 1;
-        long long ans=power(n,p/2);
+        if(p==0)
+            return 1;
+       long long ans=power(m,p/2);
         ans*=ans;
         ans%=mod;
-        if(p%2==0)return ans%mod;
+        if(p%2==0)
+            return ans%mod;
         else
-            return (ans*n)%mod;
+            return (m*ans)%mod;
     }
     int countGoodNumbers(long long n) {
         long long even=n/2;
-        long long odd=n-even;
-        return(power(5,odd)*power(4,even))%mod;
+        long long odd =n-even;
+        return (power(5,odd)*power(4,even))%mod;
         
     }
 };
