c0e7c5bb205300d2d8c8be20f90de2fe2dcfe070
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-05-16 14:26:53 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (50.79%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -11,22 +11,18 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(!head || !head->next) return head; //If there are less than 2 nodes in the given nodes, then no need to do anything just return the list as it is.
-		
-        ListNode* dummyNode = new ListNode();
-        
-        ListNode* prevNode=dummyNode;
-        ListNode* currNode=head;
-        
-        while(currNode && currNode->next){
-            prevNode->next = currNode->next;
-            currNode->next = prevNode->next->next;
-            prevNode->next->next = currNode;
-            
-            prevNode = currNode;
-            currNode = currNode->next;
+        // if head is NULL OR just having a single node, then no need to change anything 
+        if(head == NULL || head -> next == NULL) 
+        {
+            return head;
         }
+            
+        ListNode* temp; // temporary pointer to store head -> next
+        temp = head->next; // give temp what he want
         
-        return dummyNode->next;
+        head->next = swapPairs(head->next->next); // changing links
+        temp->next = head; // put temp -> next to head
+        
+        return temp; // now after changing links, temp act as our head
     }
 };
