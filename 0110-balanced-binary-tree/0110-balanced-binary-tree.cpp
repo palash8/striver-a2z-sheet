7de7f70687c69b45f58da92d0f9a6a7f1c0862fb
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-05-23 11:16:11 +0530

    Time: 13 ms (64.28%), Space: 20.9 MB (52.88%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,23 +11,23 @@
  */
 class Solution {
 public:
-    bool isBalanced(TreeNode* root) 
-    {
-        return dfsheight(root)!=-1; 
+    bool isBalanced(TreeNode* root){
+        return dfsheight(root)!=-1;
     }
-    int dfsheight(TreeNode* root)
+        
+    int dfsheight(TreeNode*root)
     {
-        if(root==NULL)
-            return 0;
+         if(root==NULL)
+             return 0;
         
-        int lh=dfsheight(root->left);
-        if(lh==-1)return-1;
+         int l=dfsheight(root->left);
+         if(l==-1)return -1; 
+         int r=dfsheight(root->right);
+         if(r==-1)return -1;
         
-        int rh=dfsheight(root->right);
-        if(rh==-1)return -1;
+         if(abs(l-r)>1)return -1;
+            
+         return 1+max(l,r);
         
-        if(abs(lh-rh)>1)return -1;
-        
-        return 1+max(lh,rh);
-    }
+     }
 };
