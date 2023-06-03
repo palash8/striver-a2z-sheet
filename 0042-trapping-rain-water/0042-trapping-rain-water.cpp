687ea9848e50e66652734eacfb538319cb01e326
Palash Yadav <43900443+palash8@users.noreply.github.com>
2023-06-03 09:10:02 +0530

    Time: 16 ms (68.50%), Space: 19.9 MB (60.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    //anuj bhaiya approach video
     int trap(vector<int>& height) {
         int n=height.size();
+        int left[n],right[n];
         int sum=0;
-        //declare 2 vectors for storing leftmost and rightmost maximum height,by takinf left from
-        //starting and right from end
-        vector<int>left(n,0);
-        vector<int>right(n,0);
+        
+        //first we calculate left side height
         left[0]=height[0];
         for(int i=1;i<n;i++)
         {
-            left[i]=max(left[i-1],height[i]);
+           left[i]=max(height[i],left[i-1]); 
         }
+        
+        //then we calculate right side height
         right[n-1]=height[n-1];
         for(int i=n-2;i>=0;i--)
         {
-            right[i]=max(right[i+1],height[i]);
+           right[i]=max(height[i],right[i+1]); 
         }
-        //now when you get both the maximum height for a particular element in left and right
-        //just use the formula below
+        
+        //total water stored
         for(int i=0;i<n;i++)
         {
-            sum+=(min(left[i],right[i])-height[i]);
+           sum+=(min(left[i],right[i])-height[i]);
         }
-        return sum;
         
+        return sum;
     }
 };
